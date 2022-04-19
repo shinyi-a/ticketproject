c67df418f6bf824d83d50cae6761969dda3889e0
Shin Yi <contact.shinyi@gmail.com>
2022-04-20 01:51:15 +0800

    set up server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  //   res.send("Hello");
+  //   res.json({ message: "wrong door" });
+  res.status(200).json({ message: "wrong door" });
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
